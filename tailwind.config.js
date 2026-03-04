bc1214b3b799dfaedf13db84035e644dd122a5ed
guchymain <145295136+guchymain@users.noreply.github.com>
2026-03-04 13:58:40 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+};
